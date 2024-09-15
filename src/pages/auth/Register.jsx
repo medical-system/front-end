aa6406069f1a30cd760f76775f544e55c44b7b03
AbdullahEltony <abdullahmeltony@gmail.com>
2024-09-15 18:57:05 +0300

    Modify Register page

---
+++ src/pages/auth/Register.jsx
@@ -0,0 +1,11 @@
+import RegisterForm from "../../views/auth/RegisterForm";
+
+const Register = () => {
+  return (
+    <div>
+      <RegisterForm />
+    </div>
+  );
+};
+
+export default Register;
