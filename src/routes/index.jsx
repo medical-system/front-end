a1edc83683ea0cdc02bc9e3b1e3fed6264d93985
AbdullahEltony <abdullahmeltony@gmail.com>
2024-09-15 18:57:56 +0300

    Modified route file

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,18 +1,19 @@
 
 import { Navigate, useRoutes } from "react-router-dom";
-import AuthLayout from "../layouts/AuthLayout.jsx";
 import RootLayout from "../layouts/RootLayout.jsx";
 import Loadable from "../Loadable.jsx";
+import Login from "../pages/auth/Login.jsx";
 import { lazy } from "react";
+import Register from "../pages/auth/Register.jsx";
 
 const Router = () => {
     return useRoutes([
         {
             path: "/auth",
-            element: <AuthLayout />,
+            element: <AuthLayoutPage />,
             children: [
-                { path: "login", element: <LoginPage /> },
-                { path: "register", element: <div>register</div> },
+                { path: "login", element: <Login /> },
+                { path: "register", element: <Register /> },
             ]
         },
         {
@@ -39,4 +40,4 @@ const Router = () => {
 
 export default Router
 
-const LoginPage = Loadable(lazy(()=>import("../pages/auth/Login.jsx")));
+const AuthLayoutPage = Loadable(lazy(()=>import("../layouts/AuthLayout.jsx")));
