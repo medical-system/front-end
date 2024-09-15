bf2d898b2eddfae0177f7ae4c9e47ea19b226ceb
AbdullahEltony <abdullahmeltony@gmail.com>
2024-09-15 18:59:12 +0300

    Add Register Auth

---
+++ src/views/auth/RegisterForm.jsx
@@ -0,0 +1,56 @@
+import FormProvider from "../../components/hook-form/FormProvider";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import RHFTextField from "../../components/hook-form/RHFTextField";
+import { BiLogInCircle } from "react-icons/bi";
+import { RegisterSchema } from "../../validation/RegisterSchema";
+
+const RegisterForm = () => {
+  const methods = useForm({
+    resolver: yupResolver(RegisterSchema),
+    defaultValues: {
+      fullName:"",
+      email: "",
+      password: "",
+    },
+  });
+
+  const {
+    reset,
+    setError,
+    handleSubmit,
+    formState: { errors },
+  } = methods;
+  const onSubmit = async (data) => {
+    try {
+      console.log(data);
+      // submit data to backend
+    } catch (error) {
+      console.log(error);
+      reset();
+      setError("afterSubmit", {
+        ...error,
+        message: error.message || error,
+      });
+    }
+  };
+  return (
+    <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+      <div spacing={3}>
+        {!!errors.afterSubmit && (
+          <p className="text-red-500">{errors.afterSubmit.message}</p>
+        )}
+      </div>
+      <div className="flex-colo gap-4">
+        <RHFTextField name="fullName" label="Full Name" type="text" />
+        <RHFTextField name="email" label="Email" type="email" />
+        <RHFTextField name="password" label="Password" type="password" />
+        <button className="w-full flex-rows gap-4 hover:opacity-80 transitions bg-subMain text-white text-sm font-medium px-2 py-4 rounded leading-[1.2]">
+          Register <BiLogInCircle size={20} className="text-xl text-white" />
+        </button>
+      </div>
+    </FormProvider>
+  );
+};
+
+export default RegisterForm;
