3ec5807369e00a59639608642e3786a07febc6cf
AbdullahEltony <abdullahmeltony@gmail.com>
2024-09-15 18:58:35 +0300

    Modify registerschema file

--- src/validation/RegisterSchema.js
+++ src/validation/RegisterSchema.js
@@ -1,7 +1,7 @@
 import * as yup from "yup";
 
 export const RegisterSchema = yup.object().shape({
-    username: yup.string().required("Username is required"),
+    fullName: yup.string().required("Full name is required"),
     email: yup.string().required("Email is required").email("Email must be a valid email address"),
     password: yup.string().required("Password is required").min(6, "Password must be at least 6 characters long"),
 })
