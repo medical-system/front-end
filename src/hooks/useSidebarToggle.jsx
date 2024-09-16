ddaf06bcc5963e1e126dd15c34a2b3575f54c8c6
AbdullahEltony <abdullahmeltony@gmail.com>
2024-09-17 00:08:47 +0300

    Add useSidebar toggle customhook

---
+++ src/hooks/useSidebarToggle.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+export const useSidebarToggle = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  useEffect(() => {
+    const hanleOpenSidebar = (e) => {
+      if (
+        !e.target.closest(".sidebar") &&
+        !e.target.classList.contains("open-btn")
+      ) {
+        setIsOpen(false);
+      }
+    };
+    document.body.addEventListener("click", hanleOpenSidebar);
+
+    return () => {
+      document.body.removeEventListener("click", hanleOpenSidebar);
+    };
+  }, [isOpen]);
+
+  return [isOpen, setIsOpen];
+};
