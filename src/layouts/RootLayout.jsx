a6bb9f1c0624f83b9b06de227b430445af3dbe32
AbdullahEltony <abdullahmeltony@gmail.com>
2024-09-17 00:08:01 +0300

    Add toggle sidebar feature

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -4,9 +4,11 @@ import { Navigate, Outlet } from "react-router-dom";
 // ** Components **
 import Sidebar from "../components/Sidebar";
 import Navbar from "../components/Navbar";
+import { useSidebarToggle } from "../hooks/useSidebarToggle";
 
 const RootLayout = () => {
   const isAuth = true;
+  const [isOpen, setIsOpen] = useSidebarToggle();
 
   if (!isAuth) {
     return <Navigate to="/auth/login" />;
@@ -14,11 +16,24 @@ const RootLayout = () => {
   return (
     // **** MAIN LAYOUT ****
     <div className="grid xl:grid-cols-12 w-full 2xl:max-w-[2000px]">
-      <div className="col-span-2 xl:block hidden">
+
+      {/* **** SIDEBAR DRAWER **** */}
+      <div
+        className={`fixed xl-hidden  ${
+          isOpen ? "block" : "hidden"
+        } w-full h-full top-0 left-0 bg-black opacity-[0.4] z-[100]`}
+      />
+
+      {/* **** SIDEBAR **** */}
+      <div
+        className={`col-span-2 xl:block xl:relative absolute ${
+          isOpen ? "block" : "hidden"
+        }`}
+      >
         <Sidebar />
       </div>
-      <main className="col-span-10 xl:h-screen overflow-y-scroll relative">
-        <Navbar />
+      <main className={`col-span-10 h-screen overflow-y-scroll relative`}>
+        <Navbar setIsOpen={setIsOpen} />
         <Outlet />
       </main>
     </div>
