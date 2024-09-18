243165ad0218fdf15bc7a446aba0b6c37b34ed76
AbdullahEltony <abdullahmeltony@gmail.com>
2024-09-18 23:18:58 +0300

    Imported aos libiry in main file

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,11 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import "aos";
+import "aos/dist/aos.css";
+
+// appexCharts 
+import "apexcharts/dist/apexcharts.css";
+
 
 createRoot(document.getElementById("root")).render(<App />);
